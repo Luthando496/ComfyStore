aa79b42b7d28b7db1ad87cddbe36b9787306f578
Luthando Didiza <luthandodidiza197@gmail.com>
2022-07-14 17:12:42 +0200

    modified the Hero Component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,21 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return(
+    <Wrapper className='section-center'>
+    <article className='content'>
+      <h1>Design Your <br/> Comfort Home</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ab molestias sint libero accusantium ex aut corporis sunt optio nulla?</p>
+      <Link to='/products' className='btn hero-btn'>
+        Shop Now
+      </Link>
+    </article>
+    <article className="img-container">
+      <img src={heroBcg} alt='Banner' className='main-img' />
+      <img src={heroBcg2} alt='Banner' className='accent-img' />
+    </article>
+    </Wrapper>
+    )
 }
 
 const Wrapper = styled.section`
